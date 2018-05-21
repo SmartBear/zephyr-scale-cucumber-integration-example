a308b0940e6d00f2739ebcea4045e46cc0215f77
Sandesh Kumar <skumar@adaptavist.com>
2018-05-21 11:51:49 +0100

    Initial Commit

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,13 @@
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features"
+        ,glue={"stepDefinition"}
+)
+
+public class TestRunner {
+
+}
